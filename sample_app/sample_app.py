144af34cd903de52c46871b24e4018c5b9707872
Diogo Osório <diogo.g.osorio@gmail.com>
2013-10-27 13:29:01 +0000

    Fixed bootstrap issues of the sample application.

--- sample_app/sample_app.py
+++ sample_app/sample_app.py
@@ -1,12 +1,12 @@
 #!/usr/env/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.appconfig import AppConfig
 from sapo_ink import Ink
 
 def create_app(configfile=None):
-    app = Flask(__name__)
+    app = Flask('sample_app')
     AppConfig(app, configfile)
 
     Ink(app)
@@ -18,4 +18,4 @@ def create_app(configfile=None):
     return app
 
 if __name__ == '__main__':
-    create_app().run(debug=True)
+    create_app('config.cfg').run(host='0.0.0.0')
