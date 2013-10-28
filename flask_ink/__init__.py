e090dfe0c2284c5475f39e725b04839192b0b97a
Diogo Osório <diogo.g.osorio@gmail.com>
2013-10-28 19:53:47 +0000

    Moved the asset logic to the assets file.

--- flask_ink/__init__.py
+++ flask_ink/__init__.py
@@ -2,69 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from flask import g, Blueprint, url_for
+from assets import LocalAssets, SapoCDN, AssetManager
 
 __version__ = '2.2.1'
-
-class AssetLocation(object):
-    def get_url_for(self, filename):
-        raise NotImplementedError
-
-
-class LocalAssets(AssetLocation):
-    def __init__(self, directory='static'):
-        self.directory = directory
-
-    def get_url_for(self, filename):
-        filename = "ink/{0}".format(filename)
-        return url_for(self.directory, filename=filename)
-
-
-class SapoCDN(AssetLocation):
-    def __init__(self, ink_version = None, base_url='//cdn.ink.sapo.pt'):
-        self.ink_version = __version__ if ink_version is None else ink_version
-        self.base_url = base_url.strip('/')
-
-    def get_url_for(self, filename):
-        return "//{url}/{version}/{filename}".format(url=self.base_url, version=self.ink_version, filename=filename)
-
-
-class AssetManager(object):
-    def __init__(
-        self, location_map={}, minified=False, asset_version=None,
-        default_location=None, append_querystring=False
-        ):
-
-        self.location_map = location_map
-        self.minified = minified
-        self.asset_version = asset_version
-        self.default_location = default_location or 'sapo'
-        self.append_querystring = append_querystring
-
-    def load(self, filename, location=None):
-        location = location or self.default_location
-        location_instance = self.get_location_by_name(location)
-
-        filename = filename.strip('/')
-
-        if self.minified:
-            filename = '%s.min.%s' % tuple(filename.rsplit('.', 1))
-
-        asset_location = location_instance.get_url_for(filename)
-
-        if self.append_querystring:
-            asset_location = '{0}?v={1}'.format(asset_location, self.asset_version)
-
-        return asset_location
-
-    def get_location_by_name(self, name):
-        if name in self.location_map:
-            return self.location_map[name]
-
-        raise ValueError("Unkown location instance {location_name}.".format(location_name=name))
-
-    def register_location(self, name, location):
-        self.location_map[name] = location
-
 
 class Ink(object):
     def __init__(self, app):
---
+++ flask_ink/assets.py
@@ -0,0 +1,78 @@
+#!/usr/env/python
+# -*- coding: utf-8 -*-
+
+class AssetLocation(object):
+    def asset_url(self, filename, minified=False, version=None):
+        raise NotImplementedError
+
+class LocalAssets(AssetLocation):
+    def __init__(self, directory='static'):
+        self.directory = directory
+
+    def asset_url(self, filename):
+        filename = "ink/{0}".format(filename)
+        return url_for(self.directory, filename=filename)
+
+class ExternalLocation(AssetLocation):
+    def __init__(self, base_url, url_pattern="//{base_url}/{filename}"):
+        self.base_url = base_url.rstrip('/')
+        self.url_pattern = url_pattern
+
+    def minified_filename(self, filename):
+        return '%s.min.%s' % tuple(filename.rsplit('.', 1))
+
+    def asset_url(self, filename, minified=False, version=None):
+        if minified:
+            filename = self.minified_filename(filename)
+
+        url = self.url_pattern
+        # TODO: generate the url from the patterns
+
+        return url
+
+class SapoCDN(ExternalLocation):
+    def minified_filename(self, filename):
+        filename_parts = filename.rsplit('.', 1)
+        extension = filename_parts[1]
+
+        pattern = '%s-min.%s' if filename_parts[1] == 'css' else '%s.min.%s'
+        return pattern % tuple(filename_parts)
+
+
+class AssetManager(object):
+    def __init__(
+        self, location_map={}, minified=False, asset_version=None,
+        default_location=None, append_querystring=False
+        ):
+
+        self.location_map = location_map
+        self.minified = minified
+        self.asset_version = asset_version
+        self.default_location = default_location or 'sapo'
+        self.append_querystring = append_querystring
+
+    def load(self, filename, location=None):
+        location = location or self.default_location
+        location_instance = self.get_location_by_name(location)
+
+        filename = filename.strip('/')
+
+        if self.minified:
+            filename = '%s.min.%s' % tuple(filename.rsplit('.', 1))
+
+        asset_location = location_instance.get_url_for(filename)
+
+        if self.append_querystring:
+            asset_location = '{0}?v={1}'.format(asset_location, self.asset_version)
+
+        return asset_location
+
+    def get_location_by_name(self, name):
+        if name in self.location_map:
+            return self.location_map[name]
+
+        raise ValueError("Unkown location instance {location_name}.".format(location_name=name))
+
+    def register_location(self, name, location):
+        self.location_map[name] = location
+
