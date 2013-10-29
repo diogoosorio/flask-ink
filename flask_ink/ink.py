2bade51eda80ed6d21d522dbf81c3ad38493cf4a
Diogo Osório <diogo.g.osorio@gmail.com>
2013-10-29 15:42:26 +0000

    Moved the __init__.py code to the ink.py file.

---
+++ flask_ink/ink.py
@@ -0,0 +1,46 @@
+#!/usr/env/python
+# -*- coding: utf-8 -*-
+
+import flask
+import assets
+
+__version__ = '2.2.1'
+
+class Ink(object):
+    def __init__(self, app):
+        self.app = app
+        self.init_app()
+
+    def init_app(self):
+        self.app.config.setdefault('INK_MINIFIED_ASSETS', False);
+        self.app.config.setdefault('INK_VERSION', __version__)
+        self.app.config.setdefault('INK_DEFAULT_ASSET_LOCATION', 'sapo')
+        self.app.config.setdefault('INK_QUERYSTRING_VERSION', False)
+
+        minified_assets = self.app.config['INK_MINIFIED_ASSETS']
+        asset_version = self.app.config['INK_VERSION']
+        asset_location = self.app.config['INK_DEFAULT_ASSET_LOCATION']
+        append_version = self.app.config['INK_QUERYSTRING_VERSION']
+
+        self.assets = AssetManager(
+            minified=minified_assets, asset_version=asset_version,
+            default_location=asset_location, append_querystring=append_version)
+
+        self.make_default_asset_locations()
+
+        blueprint = flask.Blueprint(
+            'ink',
+            __name__,
+            template_folder='templates',
+            static_folder='static',
+            static_url_path=self.app.static_url_path+'/ink')
+
+        self.app.register_blueprint(blueprint)
+        self.app.jinja_env.globals.update(ink_load_asset=self.assets.load)
+
+    def make_default_asset_locations(self):
+        sapo = SapoCDN()
+        local = LocalAssets()
+
+        self.assets.register_location('sapo', sapo)
+        self.assets.register_location('local', local)
